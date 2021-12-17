0a449f55dd68d52bf11a7645ae8db9b509e1a653
voenkomstiwe <radionpon21@gmail.com>
2021-12-17 13:41:41 +0200

    fix: add poolCard

--- src/pages/Pool/index.tsx
+++ src/pages/Pool/index.tsx
@@ -1,19 +1,13 @@
 import React from 'react';
-import { getUpperCase } from 'utils';
-import { information } from 'store';
 import teatherLogo from 'assets/images/teather.svg';
 import usdcLogo from 'assets/images/USDC.svg';
-import {
-  Container,
-  Block,
-  TokenInformation,
-  TokenContainer,
-  TokenLogo,
-  TokenTitle,
-  TokenLabel,
-  TokenValue,
-  Label,
-} from './styles';
+import PoolCard from 'component/PoolCard';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+`;
 
 const tokens = [
   {
@@ -31,33 +25,9 @@ const tokens = [
 ];
 
 export default function Pool() {
-  const label = '1 USDT ≈ 0.9992999 USDC';
-
   return (
     <Container>
-      <Block>
-        <TokenInformation>
-          {tokens.map((el) => (
-            <TokenContainer key={el.title}>
-              <TokenLogo>
-                <img src={el.img} alt={el.title} />
-              </TokenLogo>
-              <TokenTitle>
-                {el.title}
-              </TokenTitle>
-              <TokenLabel>
-                {el.label}
-              </TokenLabel>
-              <TokenValue>
-                {el.value}
-              </TokenValue>
-            </TokenContainer>
-          ))}
-        </TokenInformation>
-        <Label>
-          {label}
-        </Label>
-      </Block>
+      <PoolCard tokens={tokens} />
     </Container>
   );
 }
